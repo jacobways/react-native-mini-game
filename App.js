90128b285fb4768717c42abedb904d2e9dec2fdc
jacobways <jacobkjy0501@gmail.com>
2023-08-12 18:00:17 +0900

    feat: 앱 전체 배경색 및 그라데이션 추가

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootSceen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
+  rootSceen: {
+    flex: 1, //View 컴포넌트는 콘텐츠 크기만큼만 사이즈가 형성되기에 배경색도 그만큼 적용됨. flex 1 을 설정함으로써 모든 공간 차지하게 만들기ㄷ테ㅐ
+    // backgroundColor: "#ddb5sf",
+  },
 });
