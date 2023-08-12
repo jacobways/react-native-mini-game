f86b9affaf5f9508b7d0bfa48efc33e693b2509d
jacobways <jacobkjy0501@gmail.com>
2023-08-12 18:00:32 +0900

    feat: 앱 전체 배경색 및 그라데이션 추가

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4, // android 그림자
     shadowColor: "black", // ios 그림자
