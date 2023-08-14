a431c9a6957a33606b36b1ca0275275707d078da
jacob <jacobkjy0501@gmail.com>
2023-08-14 10:22:36 +0900

    feat: 배경이미지 추가

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
@@ -6,7 +6,14 @@ import StartGameScreen from "./screens/StartGameScreen";
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootSceen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover" // 이미지가 왜곡되지 않고 확대 축소로 모든 영역 덮기
+        style={styles.rootSceen}
+        imageStyle={styles.backgroundImages} // 특별한 props
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +22,8 @@ const styles = StyleSheet.create({
   rootSceen: {
     flex: 1, //View 컴포넌트는 콘텐츠 크기만큼만 사이즈가 형성되기에 배경색도 그만큼 적용됨. flex 1 을 설정함으로써 모든 공간 차지하게 만들기ㄷ테ㅐ
     // backgroundColor: "#ddb5sf",
+  },
+  backgroundImages: {
+    opacity: 0.15,
   },
 });
