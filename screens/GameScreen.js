b9c64b9940a0703a08b69d125e5a21bf2c354d4d
jacob <jacobkjy0501@gmail.com>
2023-08-17 12:25:05 +0900

    feat: 제목 컴포넌트

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { View, Text, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title children={"Opponent Guess"} />
       {/* Guess */}
       <View>
         <Text>Higher or lower?</Text>
