e0aadbfd8ceeb2abdc9dfc56cd8ed94b4ad76419
jacob <jacobkjy0501@gmail.com>
2023-08-23 14:27:03 +0900

    feat: gameover 화면 중첩 텍스트 적용

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,11 @@
 import { View, Image, StyleSheet, Text } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
+// Text 컴포넌트 안에 Text 컴포넌트를 추가하여 감쌀 수 있음 (Text 컴포넌트로 안에 VIEW 컴포넌트를 감싸지는 못함)
+// Text 컴포넌트에 스타일을 적용하면, 그 내에 감싸고 있는 자식 Text 컴포넌트에도 동일한 스타일 적용
+// (반면 View 컴포넌트에 style이 적용되면 자식 Text에는 해당 style이 적용되지 않음. 이것이 RN의 정석 - 계산식인 CSS와는 다름)
 function GameOverScreen() {
   return (
     <View style={styles.rootContainer}>
@@ -12,7 +16,11 @@ function GameOverScreen() {
           source={require("../assets/images/success.png")}
         />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.hightlight}>X</Text> rounds to
+        guess the number <Text style={styles.hightlight}>Y</Text>.
+      </Text>
+      <PrimaryButton text={"Start New Game"} />
     </View>
   );
 }
@@ -38,5 +46,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  hightlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
